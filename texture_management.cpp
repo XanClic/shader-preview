722e4567b9f0c69e8f887b1c27694ffb23e6e122
Hanna Reitz <xanclic@xanclic.moe>
2013-03-18 04:53:28 +0100

    Texturen laden: Qt benutzen
    * Qt kann zwar keinen Alphakanal bei Bitmaps, aber, hey.

--- texture_management.cpp
+++ texture_management.cpp
@@ -24,7 +24,7 @@ managed_texture *add_texture(unsigned id, const QString &name, bool ondemand)
     return mt;
 }
 
-static GLuint load_bmp_texture(const char *path);
+static GLuint qt_load_texture(const QString &filename);
 
 managed_texture *load_texture(const QString &path)
 {
@@ -35,15 +35,8 @@ managed_texture *load_texture(const QString &path)
     if (!path_fi.isReadable())
         return NULL;
 
-    QByteArray path_arr = path.toUtf8();
-
-    if (path_fi.suffix().toLower() == "bmp")
-    {
-        unsigned nid = load_bmp_texture(path_arr.constData());
-        return nid ? add_texture(nid, path, true) : NULL;
-    }
-
-    return NULL;
+    unsigned nid = qt_load_texture(path);
+    return nid ? add_texture(nid, path, true) : NULL;
 }
 
 void use_texture(managed_texture *tex)
@@ -88,92 +81,22 @@ int bind_texture(managed_texture *tex)
 
 
 
-static GLuint load_bmp_texture(const char *path)
+static GLuint qt_load_texture(const QString &filename)
 {
-    FILE *fp = fopen(path, "r");
-    if (fp == NULL)
+    QImage img(filename);
+
+    if (img.isNull())
         return 0;
 
-    fseek(fp, 10, SEEK_SET);
-    uint32_t ofs;
-    if (fread(&ofs, sizeof(ofs), 1, fp) < 1)
-    {
-        fclose(fp);
-        return 0;
-    }
-
-    int32_t w, h;
-    fseek(fp, 4, SEEK_CUR);
-    if ((fread(&w, sizeof(w), 1, fp) < 1) || (fread(&h, sizeof(h), 1, fp) < 1))
-    {
-        fclose(fp);
-        return 0;
-    }
-
-    uint16_t bpp;
-    fseek(fp, 2, SEEK_CUR);
-
-    if ((fread(&bpp, sizeof(bpp), 1, fp) < 1) || ((bpp != 24) && (bpp != 32)))
-    {
-        fclose(fp);
-        return 0;
-    }
+    img = img.convertToFormat(QImage::Format_ARGB32);
 
 
-    fseek(fp, 0, SEEK_END);
-    long lof = ftell(fp);
-    fseek(fp, ofs, SEEK_SET);
+    int w = img.width(), h = img.height();
 
+    uint8_t *buf = new uint8_t[w * h * 4];
 
-    unsigned char *content = new unsigned char[lof - ofs];
-    if ((long)fread(content, 1, lof - ofs, fp) < lof - (long)ofs)
-    {
-        delete content;
-        fclose(fp);
-        return 0;
-    }
-    fclose(fp);
-
-
-    bool neg_h = h < 0;
-
-    if (neg_h)
-        h = -h;
-
-    unsigned char *buf = new unsigned char[w * h * 4];
-
-
-    int i = 0, o = 0;
-
-    if (!neg_h)
-    {
-        for (int y = 0; y < h; y++)
-        {
-            for (int x = 0; x < w; x++)
-            {
-                buf[o++] = content[i++];
-                buf[o++] = content[i++];
-                buf[o++] = content[i++];
-                buf[o++] = (bpp == 32) ? content[i++] : 0;
-            }
-        }
-    }
-    else
-    {
-        for (int y = h - 1; y >= 0; y--)
-        {
-            for (int x = 0; x < w; x++)
-            {
-                // FIXME: I don't know what, but it certainly needs to be fixed
-                int op = (y * w + x) * 4;
-
-                buf[op++] = content[i++];
-                buf[op++] = content[i++];
-                buf[op++] = content[i++];
-                buf[op++] = (bpp == 32) ? content[i++] : 0;
-            }
-        }
-    }
+    for (int y = 0; y < h; y++)
+        memcpy(&buf[(h - y - 1) * w * 4], img.constScanLine(y), w * 4);
 
 
     GLuint tex_id;
@@ -186,7 +109,6 @@ static GLuint load_bmp_texture(const char *path)
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, buf);
 
-    delete content;
     delete buf;
 
 
